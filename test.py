b997fd293f06ff6e35514eebbede4548de9d1fe7
Jim Zhang <dqzhw@126.com>
2020-02-17 19:19:03 +0800

    Delete test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-import en_wikipedia_toolkit
-spider=en_wikipedia_toolkit.spider()
-spider.billboard('https://www.billboard.com/articles/news/8551157/chance-the-rapper-2020-nba-all-star-game-halftime-show')
